470a684a6f94d77321ffa373007d5fb071bd52bc
Cade Chaney <cadechaney@live.com>
2023-05-11 21:05:32 -0500

    Add single movie component

---
+++ src/Components/Movies/singleMovie.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+const SingleMovie = (filteredMovie) => {
+  console.log(filteredMovie.filteredMovie[0].title)
+  return (
+    <div>
+      <img className='single-poster' src={filteredMovie.filteredMovie[0].backdrop_path} ></img>
+      <div>
+        <p>{filteredMovie.filteredMovie[0].title}</p>
+        <p>{filteredMovie.filteredMovie[0].average_rating}</p>
+      </div>
+    </div>
+  )
+}
+
+export default SingleMovie;
