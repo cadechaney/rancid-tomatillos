4ef70f97f5eb7f27ec75faf0b9400f29c2b270a8
Michael Lobato <mike.lobato.wft@gmail.com>
2023-05-13 11:22:37 -0700

    Add: button and class names

--- src/Components/Movies/singleMovie.js
+++ src/Components/Movies/singleMovie.js
@@ -3,11 +3,12 @@ import React from 'react'
 
 const SingleMovie = (filteredMovie) => {
   return (
-    <div>
+    <div className='single-movie' >
       <img className='single-poster' src={filteredMovie.filteredMovie[0].backdrop_path} ></img>
-      <div>
+      <div className='text-details'>
         <p>{filteredMovie.filteredMovie[0].title}</p>
         <p>{filteredMovie.filteredMovie[0].average_rating}</p>
+        <button className='return-button'>Return</button>
       </div>
     </div>
   )
